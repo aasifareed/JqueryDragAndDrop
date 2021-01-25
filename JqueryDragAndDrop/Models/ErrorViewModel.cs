63bb541892ac3b61d1d103c0d727c9d516ab2410
Muhammad Asif <muhammad.asif@estratcom.com>
2021-01-25 10:51:24 +0500

    Add project files.

---
+++ JqueryDragAndDrop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JqueryDragAndDrop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
